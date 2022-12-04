2215358c16ea03e212173c2077868339b57458d9
Justin Nguyen <justinnguyen2245@gmail.com>
2022-12-04 06:13:24 -0800

    methods and comments

--- GraphMatrix.java
+++ GraphMatrix.java
@@ -1,3 +1,64 @@
+/**
+ * Graph using a 2d array implementation / adjacency matrix
+ */
 public class GraphMatrix<E>{
-	//all in slides 
+	private boolean[][] edges;
+	private E[] labels;
+
+	/**
+	 * Constructs graph of n vertices
+	 * @param n number of vertices
+	 */
+	public GraphMatrix(int n){
+		edges = new boolean[n][n];
+		labels = (E[]) new Object[n];
+	}
+
+	/**
+	 * @param vertex an int representation of vertex
+	 * @return label of vertex
+	 */
+	public E getLabel(int vertex){
+		return labels[vertex];
+	}
+
+	public boolean isEdge(int source, int target){
+		return edges[source][target];
+	}
+
+	public void addEdge(int source, int target){
+		edges[source][target] = true;
+	}
+
+	public int[] neighbors(int vertex){
+		int i;
+		int count = 0;
+		int[] answer;
+		for(int i=0; i<labels.length; i++){
+			if(edges[vertex][i]){
+				count++;
+			}
+		}
+		answer = new int[count];
+		count = 0;
+		for(int i=0; i<labels.length; i++){
+			if(edges[vertex][i]){
+				answer[count] = i;
+				count++;
+			}
+		}
+		return answer;
+	}
+
+	public void removeEdge(int source, int target){
+		edges[source][target] = false;
+	}
+
+	public void setLabel(int vertex, E newLabel){
+		labels[vertex] = newLabel;
+	}
+
+	public int size(){
+		return labels.length;
+	}
 }
