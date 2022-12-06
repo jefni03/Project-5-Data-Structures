9335779e38d75823fad2baed601f1d87c22e21b1
Justin Nguyen <justinnguyen2245@gmail.com>
2022-12-05 23:19:56 -0800

    added methods

--- GraphList.java
+++ GraphList.java
@@ -22,8 +22,80 @@ public class GraphList<T> implements GraphInterface<T>
 		edgeCount = 0;
 	} // end default constructor
 
-/* Implementations of the graph operations go here. 
-   . . . */
+
+	public boolean addEdge(T begin, T end, double edgeWeight)
+	{
+	   boolean result = false;
+	   VertexInterface<T> beginVertex = vertices.getValue(begin);
+	   VertexInterface<T> endVertex = vertices.getValue(end);
+	   if ( (beginVertex != null) && (endVertex != null) )
+		  result = beginVertex.connect(endVertex, edgeWeight);
+	   if (result)
+		  edgeCount++;
+	   return result;
+	} // end addEdge
+	
+	public boolean addEdge(T begin, T end)
+	{
+	   return addEdge(begin, end, 0);
+	} // end addEdge
+
+
+	public boolean hasEdge(T begin, T end)
+	{
+	   boolean found = false;
+	   VertexInterface<T> beginVertex = vertices.getValue(begin);
+	   VertexInterface<T> endVertex = vertices.getValue(end);
+	   if ( (beginVertex != null) && (endVertex != null) )
+	   {
+		  Iterator<VertexInterface<T>> neighbors = beginVertex.getNeighborIterator();
+		  while (!found && neighbors.hasNext())
+		  {
+			 VertexInterface<T> nextNeighbor = neighbors.next();
+			 if (endVertex.equals(nextNeighbor))
+				found = true;
+		  } // end while
+	   } // end if
+	   return found;
+	} // end hasEdge
+
+
+	public boolean isEmpty()
+	{
+	   return vertices.isEmpty();
+	} // end isEmpty
+	
+	public void clear()
+	{
+	   vertices.clear();
+	   edgeCount = 0;
+	} // end clear
+	
+	public int getNumberOfVertices()
+	{
+	   return vertices.getSize();
+	} // end getNumberOfVertices
+	
+	public int getNumberOfEdges()
+	{
+	   return edgeCount;
+	} // end getNumberOfEdges
+
+
+	protected void resetVertices()
+	{
+	   Iterator<VertexInterface<T>> vertexIterator = vertices.getValueIterator();
+	   while (vertexIterator.hasNext())
+	   {
+		  VertexInterface<T> nextVertex = VertexIterator.next();
+		  nextVertex.unvisit();
+		  nextVertex.setCost(0);
+		  nextVertex.setPredecessor(null);
+	   } // end while
+	} // end resetVertices
+
+
+
    public QueueInterface <T> getBreadthFirstTraversal(T origin)
    {
 	   resetVertices();
